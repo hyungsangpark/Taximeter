667b45ec8de251e81dad92ceca9d295de9bdf47d
hyungsangpark <0916bacon@gmail.com>
2021-02-28 23:33:07 +1300

    Initial Commit

---
+++ Taximeter/TaximeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaximeterApp.swift
+//  Taximeter
+//
+//  Created by 박형상 on 28/02/21.
+//
+
+import SwiftUI
+
+@main
+struct TaximeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
